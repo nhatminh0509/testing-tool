02659608b08c5e5a489329646e74c18833c379fa
nhatminh0509 <minhnhat@bacoor.co>
2022-06-03 13:20:16 +0700

    update

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
+const path = require('path')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules[3].oneOf.forEach((one) => {
+      if (!`${one.issuer?.and}`.includes('_app')) return;
+      one.issuer.and = [path.resolve(__dirname)];
+    });
+    return config;
+  },
 }
 
 module.exports = nextConfig
